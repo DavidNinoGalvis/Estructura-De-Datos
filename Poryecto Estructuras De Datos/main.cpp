6fb27ea3eeb70e01ead08e611dcb6e53816eb921
DavidNinoGalvis <110640743+DavidNinoGalvis@users.noreply.github.com>
2023-05-07 00:03:58 -0500

    Entrega 1 - Proyecto Final Estructuras de Datos
    Entrega 1 - Proyecto Final Estructuras de Datos
    Implementacion TAD de Big Integer

---
+++ Poryecto Estructuras De Datos/main.cpp
@@ -0,0 +1,20 @@
+/*
+Proyecto Final Estructura de Datos - BIg Integer
+Profesor: Carlos Ramirez
+Estudiante: David Nino Galvis
+Fecha: 2 de Mayo del 2023
+ */
+
+#include <iostream>
+#include "bigInteger.h"
+using namespace std;
+
+int main(){
+    //string text1 = "4545154";
+    string cad1 = "12345678901234567890";
+    string cad2 = "9876543210987654321";
+    //bigInteger bigExample;
+    //bigExample.stringToBigInt(text1);
+
+    return 0;
+}
